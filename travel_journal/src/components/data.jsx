4cb861ce5907822984e4ed5efa39b02530284514
Tom Gagliano <tgaglianoll@gmail.com>
2024-03-26 21:31:13 -0400

    add another object to data.js file for Positano, Italy

--- travel_journal/src/components/data.jsx
+++ travel_journal/src/components/data.jsx
@@ -9,4 +9,16 @@ export default [
       "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
     imageUrl: "https://source.unsplash.com/WLxQvbMyfas",
   },
+  {
+    title: "Positano",
+    location: "Italy",
+    googleMapsUrl:
+      "https://www.google.com/maps/place/84017+Positano,+SA,+Italy/@40.6136427,14.3908806,12z/data=!3m1!4b1!4m6!3m5!1s0x133b976f8f2d742f:0xc0cbeaa3312869bb!8m2!3d40.6285801!4d14.4846221!16zL20vMDF5Ymo5?entry=ttu",
+    startDate: "12 Feb, 2016",
+    endDate: "20 Jan, 2016",
+    description:
+      "Positano is famous for being a scenic seaside paradise in Italy. You'll be blown away with the colourful, cliffside villas overlooking the sunny Amalfi Coast.",
+    imageUrl:
+      "https://a.cdn-hotels.com/gdcs/production144/d547/b9cfd8e5-212f-4945-8010-b76f326ccd18.jpg?impolicy=fcrop&w=800&h=533&q=medium",
+  },
 ];
